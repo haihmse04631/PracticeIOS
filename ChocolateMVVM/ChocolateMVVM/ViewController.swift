bff1284c35d397aee39c747468abd89484452fe2
HoangHai <hoangminhhai1997@gmail.com>
2018-08-09 22:38:08 +0700

    add Chocolate MVVM

---
+++ ChocolateMVVM/ChocolateMVVM/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ChocolateMVVM
+//
+//  Created by Hoàng Hải on 8/7/18.
+//  Copyright © 2018 Macbook Pro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
